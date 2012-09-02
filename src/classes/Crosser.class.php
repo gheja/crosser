6e879d10e109c8f26aea12e8ec5edd878c1579a8
Gabor Heja <gheja@bagheera>
2012-09-03 01:08:59 +0200

    Implemented some prechecks of configuration in Crosser class before doing any real work.

--- src/classes/Crosser.class.php
+++ src/classes/Crosser.class.php
@@ -1,4 +1,4 @@
-<?php // 2
+<?php // 3
 	require_once(CROSSER_ROOT_DIR . "/classes/CrosserException.class.php");
 	require_once(CROSSER_ROOT_DIR . "/classes/CrosserLib.class.php");
 	
@@ -6,6 +6,14 @@
 	{
 		private $accounts;
 		private $routes;
+		private $plugin_definitions; // kind of...
+		
+		public function __construct()
+		{
+			$this->plugin_definitions = array(
+				"twitter" => array("classname" => "CrosserTwitter", "files" => array("/classes/CrosserTwitter.class.php")),
+			);
+		}
 		
 		private function SetAccounts($accounts)
 		{
@@ -36,9 +44,53 @@
 			$this->SetRoutes($routes);
 		}
 		
+		public function DoThePrechecks()
+		{
+			foreach ($this->routes as $route_name => $route)
+			{
+				/* TODO: these checks could be moved to various methods, like AccountExists(), PluginExists(), ... */
+				
+				/* check the "from" account, before doing actual work */
+				$from = $route['from'];
+				
+				if (!array_key_exists($this->accounts, $from['account']))
+				{
+					trhow new CrosserException("Account \"" . $from['account'] . "\" does not exist, it was referenced in route \"". $route_name "\" as \"from\".");
+				}
+				
+				if (!array_key_exists($this->plugin_definitions, $this->accounts[$from['account']]['type']))
+				{
+					trhow new CrosserException("Could not find plugin definition for account type \"" . $this->accounts[$from['account']]['type'] . "\", referenced by account \"" . $from['account'] . "\", referenced in route \"". $route_name "\" as \"from\".");
+				}
+				
+				/* check the "to" account(s), before doing actual work */
+				foreach ($route['to'] as $to_id => $to)
+				{
+					if (!array_key_exists($this->accounts, $to['account']))
+					{
+						trhow new CrosserException("Account \"" . $to['account'] . "\" does not exist, it was referenced in route \"". $route_name "\" as \"to\".");
+					}
+					
+					if (!array_key_exists($this->plugin_definitions, $this->accounts[$to['account']]['type']))
+					{
+						trhow new CrosserException("Could not find plugin definition for account type \"" . $this->accounts[$to['account']]['type'] . "\", referenced by account \"" . $to['name'] . "\", referenced in route \"". $route_name "\" as \"from\".");
+					}
+				}
+			}
+			
+			return true;
+		}
+		
 		public function DoTheWork()
 		{
+			if (!$this->DoThePrechecks())
+			{
+				throw new CrosserException("DoThePrechecks() returned false, something might be wrong."); // not a too informative message, hopefully will be never shown as every failure should throw an exception.
+			}
 			
+			foreach ($this->routes as $route_name => $route)
+			{
+			}
 		}
 	}
 ?>
